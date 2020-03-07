316f296f20cfcf9d0e63601ae16f99339ccfc71f
Gael <gael@shippabo.com>
2020-03-07 02:00:46 +0100

    Fix variable name

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = {
   env: {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    SERVER_URL: process.env.NOW_URL || 'http://localhost:3000'
+    SERVER_URL:
+      process.env.NOW_URL || process.env.SERVER_URL || 'http://localhost:3000'
   }
 };
